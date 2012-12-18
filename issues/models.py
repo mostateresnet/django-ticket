201235b0085a6bd1f6d42ceebe1aec065813fe81
Seth <seth_thoenen@hotmail.com>
2012-12-18 13:29:50 -0600

    Truncated completed issues to 10 for the dashboard

--- issues/models.py
+++ issues/models.py
@@ -146,7 +146,7 @@ class UserMethods(User):
         return self.issue_set.filter(status='IP')
 
     def completed_issues(self):
-        return self.issue_set.filter(status='CP').order_by('-close_date')
+        return self.issue_set.filter(status='CP').order_by('-close_date')[:10]
 
     def last_completed(self):
         completed = self.issue_set.filter(status='CP').order_by('-close_date')
