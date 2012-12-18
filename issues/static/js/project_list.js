4944fe5a2f480d3d1568ec57cdeb3bf75c575bdb
Seth <seth_thoenen@hotmail.com>
2012-12-18 15:21:40 -0600

    Dashboard styling overhaul

--- issues/static/js/project_list.js
+++ issues/static/js/project_list.js
@@ -38,6 +38,86 @@ $(function()
         }
         return false;
     });
+    
+    $(".project-tab").click(function(e)
+    {
+        //get the clicked id
+        //$(this).id returns null here for some reason
+        var target_id = event.target.id;
+        
+        //this determines which tab was clicked (pojects, in-progress, assigned, unassigned, needs-review, completed, or deleted)
+        var tab = target_id.slice(9);
+        
+        //remove active tab
+        $(".project-tab").each(function(i){
+            $(this).removeClass("active");
+        });
+        
+        //assign active tab
+        $("#"+target_id).addClass("active");
+        
+        if (tab == "projects")
+        {
+            $("#project-issues").hide();
+            $("#project-sort").show();
+        }
+        //show issues based on the tab clicked        
+        else
+        {
+            $("#project-sort").hide();
+            $("#project-issues").show();
+            $(".project-issue").each(function(i){
+            if ($(this).hasClass(tab))
+                {
+                    $(this).show();
+                }
+                else
+                {
+                    $(this).hide();
+                }
+            });
+        }        
+    });
+    
+    $(".user-tab").click(function(e)
+    {
+        //get the clicked id
+        //$(this).id returns null here for some reason
+        var target_id = event.target.id;
+        
+        //this determines which tab was clicked (pojects, in-progress, assigned, unassigned, needs-review, completed, or deleted)
+        var tab = target_id.slice(6);
+        
+        //remove active tab
+        $(".user-tab").each(function(i){
+            $(this).removeClass("active");
+        });
+        
+        //assign active tab
+        $("#"+target_id).addClass("active");
+        
+        if (tab == "reorder")
+        {
+            $("#user-issues").hide();
+            $("#user-sort").show();
+        }
+        //show issues based on the tab clicked        
+        else
+        {
+            $("#user-sort").hide();
+            $("#user-issues").show();
+            $(".user-issue").each(function(i){
+            if ($(this).hasClass(tab))
+                {
+                    $(this).show();
+                }
+                else
+                {
+                    $(this).hide();
+                }
+            });
+        }        
+    });
 
     $(document).ready(function() 
     {
