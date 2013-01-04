40ae5277f67f7094e855839ca77da9e24db9e92a
Shane Allgeier <shaneallgeier@gmail.com>
2013-01-04 14:17:27 -0600

    Began work on unit tests

--- issues/tests.py
+++ issues/tests.py
@@ -1,16 +1,16 @@
 """
 This file demonstrates writing tests using the unittest module. These will pass
 when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from django.core.urlresolvers import reverse
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
+class ProjectListViewTest(TestCase):
+    def test_project_list_responds_200(self):
         """
-        Tests that 1 + 1 always equals 2.
+        Tests that the ProjectListView responds with 200 OK.
         """
-        self.assertEqual(1 + 1, 2)
+        response = self.client.get(reverse('project_list'))
+        self.assertEqual(response.status_code, 200, "ProjectListView should respond with HTTP 200 OK")
