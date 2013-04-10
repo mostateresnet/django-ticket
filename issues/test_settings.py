d005ef738d72970221f85113198c322a6602a519
Shane Allgeier <shaneallgeier@gmail.com>
2013-04-10 17:16:06 -0500

    Added some more Travis-CI builds

---
+++ issues/test_settings.py
@@ -0,0 +1,24 @@
+TEST_RUNNER = 'discover_runner.DiscoverRunner'
+ROOT_URLCONF = 'issues.urls'
+STATIC_URL = '/static/'
+USE_TZ = True
+SECRET_KEY = 'CHANGE_THIS_TO_SOMETHING_UNIQUE_AND_SECURE'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'issues.sqlite3',                # Or path to database file if using sqlite3.
+    },
+}
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.admin',
+    'issues',
+    'south',
+)
